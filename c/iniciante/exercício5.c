6b22c2f3865db23da826b2a9c694c74ad748edc3
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:12:32 -0300

    Update exercício5.c

--- c/iniciante/exercício5.c
+++ c/iniciante/exercício5.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main() {
+ int idade;
+ printf("Digite a idade: ");
+ scanf("%d", &idade);
+ if (idade >= 18 && idade <= 30) {
+ printf("Esta entre 18 e 30 anos\n");
+ } else {
+ printf("Fora da faixa\n");
+ }
+ return 0;
+}
+�
